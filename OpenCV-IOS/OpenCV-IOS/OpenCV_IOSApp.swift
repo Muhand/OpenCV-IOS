b26d09bd3941514297f7bd591f22ade152815d4b
Muhand Jumah <muhandjumah@gmail.com>
2020-11-24 01:42:11 -0500

    Initialized project

---
+++ OpenCV-IOS/OpenCV-IOS/OpenCV_IOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  OpenCV_IOSApp.swift
+//  OpenCV-IOS
+//
+//  Created by Muhand Jumah on 11/24/20.
+//
+
+import SwiftUI
+
+@main
+struct OpenCV_IOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
